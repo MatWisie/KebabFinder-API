b90bb44167090b9e69253385d8a3633d96ce198f
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-11-01 15:37:01 +0100

    added tailwind

--- KebabFrontend/src/Pages/LogInPage.js
+++ KebabFrontend/src/Pages/LogInPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function LogInPage() {
+
+    
+
     return(
         <div>
-            <h1>Welcome, Admin</h1>
+            <h1 className="text-3xl font-bold underline">Welcome, Admin</h1>
             <form>
                 <input type="email" placeholder='Email'></input>
                 <input type="password" placeholder='Password'></input>
