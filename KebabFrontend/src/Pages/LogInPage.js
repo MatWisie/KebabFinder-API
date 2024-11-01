6de97bd7624a2b00f88f7f44d6df6da3aa92d1da
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-11-01 16:17:24 +0100

    swaped email input with username

--- KebabFrontend/src/Pages/LogInPage.js
+++ KebabFrontend/src/Pages/LogInPage.js
@@ -11,8 +11,8 @@ export default function LogInPage() {
             <form className="flex flex-col space-y-4">
               <div>
                 <input 
-                  type="email" 
-                  placeholder="Email" 
+                  type="test" 
+                  placeholder="Username" 
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
               </div>
