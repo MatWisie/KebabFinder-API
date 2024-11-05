377df3a302f6c9ad3b97b6d7b5f38b7a518992c2
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-05 16:19:48 +0100

    Services for routes for logging users and admins

--- KebabBackend/app/Services/UserAuthService.php
+++ KebabBackend/app/Services/UserAuthService.php
@@ -5,13 +5,13 @@ namespace App\Services;
 use App\Models\User;
 use Illuminate\Validation\ValidationException;
 
-class AdminAuthService
+class UserAuthService
 {
     public function authenticate(array $credentials): void
     {
         $user = User::where("email", $credentials["email"])->first();
 
-        if (!$user || !$user->is_admin) {
+        if (!$user || $user->is_admin) {
             throw ValidationException::withMessages([
                 'email' => __('auth.failed'),
             ]);
