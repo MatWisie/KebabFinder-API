60d27ae6c99b6479e60e444d2d4ee85519a10fee
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-05 16:21:33 +0100

    Added functions for user and admin routes

--- KebabBackend/app/Http/Controllers/UserController.php
+++ KebabBackend/app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use \App\Models\User;
-use Response;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class UserController extends Controller
 {
@@ -34,5 +36,57 @@ class UserController extends Controller
     {
         $users = User::all();
         return response()->json($users);
+    }
+
+    public function changeUsername(Request $request): JsonResponse
+    {
+        $user = auth()->user();
+
+        $request->validate([
+            'name' => 'required|string|max:255|unique:users,name,' . $user->id,
+        ]);
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        return response()->json(['message' => 'Username updated successfully', 'user' => $user], 200);
+    }
+
+    public function changePassword(Request $request): JsonResponse
+    {
+        $user = auth()->user();
+
+        $request->validate([
+            'current_password' => 'required|string',
+            'new_password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        if (!\Hash::check($request->input('current_password'), $user->password)) {
+            return response()->json(['message' => 'Current password is incorrect'], 403);
+        }
+
+        $user->password = bcrypt($request->input('new_password'));
+        $user->save();
+
+        return response()->json(['message' => 'Password updated successfully'],200);
+    }
+
+    public function changePasswordForFirstLogin(Request $request): JsonResponse
+    {
+        $user = auth()->user();
+
+        if (! $user->is_first_login) {
+            return response()->json(['message'=> 'Already changed password'], 403);
+        }
+
+        $request->validate([
+            'new_password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user->password = Hash::make($request->input('new_password'));
+        $user->is_first_login = false;
+        $user->save();
+
+        return response()->json(['message' => 'Password changed successfully.'], 200);
     }
 }
