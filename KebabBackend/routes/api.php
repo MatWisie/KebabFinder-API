67c44d013cd53b6b34837dfae08d82e2fa74ca58
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-05 16:18:13 +0100

    Added routes
    get all users
    delete user by id
    change account name
    change account password
    change password for first login admin

--- KebabBackend/routes/api.php
+++ KebabBackend/routes/api.php
@@ -8,8 +8,14 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum'])->get('/users', [UserController::class, 'index']);
+Route::middleware(['auth:sanctum','admin'])->get('/users', [UserController::class, 'index']);
 
 Route::middleware(['auth:sanctum'])->get('/first-login', [UserController::class, 'isFirstLogin']);
 
-Route::middleware(['auth:sanctum','admin'])->delete('/delete-user', [UserController::class, 'destroy']);
+Route::middleware(['auth:sanctum','admin'])->delete('/delete-user/{id}', [UserController::class, 'destroy']);
+
+Route::middleware(['auth:sanctum'])->put('/user/change-username', [UserController::class, 'changeUsername']);
+
+Route::middleware(['auth:sanctum'])->post('/user/change-password', [UserController::class, 'changePassword']);
+
+Route::middleware(['auth:sanctum', 'admin'])->post('/admin/change-password-first-login', [UserController::class, 'changePasswordForFirstLogin']);
