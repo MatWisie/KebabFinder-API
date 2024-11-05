0811bc87f3b558fa4fb85dce9158d642a8e60ecb
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-05 02:32:53 +0100

    Update api.php

--- KebabBackend/routes/api.php
+++ KebabBackend/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->get('/users', [UserController::class, 'index']);
+
+Route::middleware(['auth:sanctum'])->get('/first-login', [UserController::class, 'isFirstLogin']);
+
+Route::middleware(['auth:sanctum','admin'])->delete('/delete-user', [UserController::class, 'destroy']);
