143ee0a66ffe04b4361807f57e97a2753ed16607
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-05 16:25:57 +0100

    Added json tith token to response

--- KebabBackend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ KebabBackend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,19 +7,38 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Services\UserAuthService;
 
 class AuthenticatedSessionController extends Controller
 {
+    protected $userAuthService;
+
+    public function __construct(UserAuthService $userAuthService)
+    {
+        $this->userAuthService = $userAuthService;
+    }
+
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
+        $this->userAuthService->authenticate($request->only("email"));
+
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user = $request->user();
+
+        $user->tokens()->delete();
+
+        $token = $user->createToken('api-token');
+
+        return response()->json([
+            'token'=> $token,
+        ]);
     }
 
     /**
