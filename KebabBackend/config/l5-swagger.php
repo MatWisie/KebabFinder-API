b10a11d8973aa74dfcdc48298bb13593e905f476
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-09 20:59:09 +0100

    fix for swagger

--- KebabBackend/config/l5-swagger.php
+++ KebabBackend/config/l5-swagger.php
@@ -67,7 +67,7 @@ return [
             'middleware' => [
                 'api' => [],
                 'asset' => [],
-                'docs' => ['auth:api'],
+                'docs' => [],
                 'oauth2_callback' => [],
             ],
 
