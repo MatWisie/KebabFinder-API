6b61f31609a516525dc1730f50b5bf432d6cd66a
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-09 22:17:40 +0100

    Create KebabController.php

---
+++ KebabBackend/app/Http/Controllers/Api/KebabController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Kebab;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class KebabController extends Controller
+{
+
+}
