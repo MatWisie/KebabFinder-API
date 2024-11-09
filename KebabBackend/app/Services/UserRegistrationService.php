f3cf6c903644434570217fca1dcd290ddd1e3561
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-09 19:21:48 +0100

    Update UserRegistrationService.php

--- KebabBackend/app/Services/UserRegistrationService.php
+++ KebabBackend/app/Services/UserRegistrationService.php
@@ -4,9 +4,10 @@ namespace App\Services;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
 
 class UserRegistrationService
 {
@@ -15,7 +16,7 @@ class UserRegistrationService
         $validator = Validator::make($data, [
             'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed', Password::defaults()],
+            'password' => ['required', 'string', 'confirmed', Password::defaults()],
         ]);
 
         if ($validator->fails()) {
@@ -27,6 +28,8 @@ class UserRegistrationService
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        event(new Registered($user));
 
         return $user->createToken('API Token')->plainTextToken;
     }
