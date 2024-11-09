e7adac9c77225d3649ef910ccec74149afc7a165
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-09 19:51:57 +0100

    Deleted not needed import

--- KebabBackend/routes/api.php
+++ KebabBackend/routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
