01bbeef6824fa921552998ee9d6c9b2f40108e80
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-09 19:21:11 +0100

    Deleted unused code

--- KebabBackend/config/l5-swagger.php
+++ KebabBackend/config/l5-swagger.php
@@ -67,7 +67,7 @@ return [
             'middleware' => [
                 'api' => [],
                 'asset' => [],
-                'docs' => [],
+                'docs' => ['auth:api'],
                 'oauth2_callback' => [],
             ],
 
