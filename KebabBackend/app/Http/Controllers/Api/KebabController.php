79a75a2bda456ae28d8a08bb8d7974ccce892e50
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-10 13:04:12 +0100

    Function for adding kebabs

--- KebabBackend/app/Http/Controllers/Api/KebabController.php
+++ KebabBackend/app/Http/Controllers/Api/KebabController.php
@@ -13,6 +13,59 @@ class KebabController extends Controller
     {
         $kebab = Kebab::with(['openingHour', 'meatTypes', 'orderWay', 'sauces', 'socialMedias'])->paginate(10);
 
+    public function store(Request $request): JsonResource
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'address' => 'required|string',
+            'coordinates' => 'required|string',
+            'sauces' => 'array|exists:saucetypes,id',
+            'meats' => 'array|exists|meattypes,id',
+            'social_media_links' => 'array',
+            'opening_hours' => 'array',
+            'order_ways' => 'array'
+        ]);
+
+        $kebab = Kebab::create($validated);
+
+        foreach ($validated['sauces'] as $sauceId) {
+            SauceType::create([
+                'kebab_id' => $kebab->id,
+                'sauce_id' => $sauceId
+            ]);
+        }
+
+        foreach ($validated['meats'] as $meatId) {
+            MeatType::create([
+                'kebab_id' => $kebab->id,
+                'meat_type_id' => $meatId
+            ]);
+        }
+
+        foreach ($validated['social_media_links'] as $link) {
+            KebabSocialMedia::create([
+                'kebab_id' => $kebab->id,
+                'social_media_link' => $link
+            ]);
+        }
+
+        foreach ($validated['opening_hours'] as $day => $hours) {
+            OpeningHour::create([
+                'kebab_id' => $kebab->id,
+                $day . '_open' => $hours['open'] ?? null,
+                $day . '_close' => $hours['close'] ?? null,
+            ]);
+        }
+
+        foreach ($validated['order_ways'] as $way) {
+            OrderWay::create([
+                'kebab_id' => $kebab->id,
+                'app_name' => $way['app_name'] ?? null,
+                'phone_number' => $way['phone_number'] ?? null,
+                'website' => $way['website'] ?? null,
+            ]);
+        }
+
         return response()->json($kebab);
     }
 }
