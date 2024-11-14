8788c8eef5536dd004f53c10e672ea11dd47f25b
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-14 23:23:45 +0100

    Add comment function

--- KebabBackend/app/Http/Controllers/Api/CommentController.php
+++ KebabBackend/app/Http/Controllers/Api/CommentController.php
@@ -5,4 +5,18 @@ namespace App\Http\Controllers\Api;
 
 class CommentController extends Controller
 {
+    public function addComment(Request $request, Kebab $kebab): JsonResponse
+    {
+        $validated = $request->validated();
+
+        $comment = Comment::create([
+            'user_id' => Auth::id(),
+            'content' => $request->input('content'),
+        ]);
+
+        $kebab->comments()->save($comment);
+
+        return response()->json(['message' => 'Comment added successfully', 'comment' => $comment], 201);
+    }
+
 }
