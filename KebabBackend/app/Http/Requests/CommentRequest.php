f523bd9762733bb5dcea61e1c1cd7d012ac8c5a3
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-14 23:25:31 +0100

    Update CommentRequest.php

--- KebabBackend/app/Http/Requests/CommentRequest.php
+++ KebabBackend/app/Http/Requests/CommentRequest.php
@@ -6,4 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CommentRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 }
