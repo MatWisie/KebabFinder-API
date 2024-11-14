9848ef42251104dcea84e5b09bcfaaa54f2e9129
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-14 23:26:37 +0100

    update policy

--- KebabBackend/app/Policies/CommentPolicy.php
+++ KebabBackend/app/Policies/CommentPolicy.php
@@ -4,4 +4,11 @@ namespace App\Policies;
 
 class CommentPolicy
 {
+    /**
+     * Determine if the authenticated user can edit a comment.
+     */
+    public function update(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user_id;
+    }
 }
