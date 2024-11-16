f526540dba3055fdb3cdd81b685e7bb4f5387d0e
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-16 21:17:39 +0100

    imports for favourite controller

--- KebabBackend/app/Http/Controllers/Api/FavouriteController.php
+++ KebabBackend/app/Http/Controllers/Api/FavouriteController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kebab;
+use Illuminate\Http\JsonResponse;
+use App\Services\FavouriteService;
 
 class FavouriteController extends Controller
 {
