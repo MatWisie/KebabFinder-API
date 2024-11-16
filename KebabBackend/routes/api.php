b696a7d5a9978b6253f2bf074deb7bf90baa63bb
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-16 21:21:32 +0100

    routes for favourite kebabs controller

--- KebabBackend/routes/api.php
+++ KebabBackend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\FavouriteController;
 use App\Http\Controllers\Api\KebabController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
@@ -23,6 +24,8 @@ Route::middleware(['auth:sanctum'])->prefix('user')->group(function () {
     Route::put('change-username', [UserController::class, 'changeUsername']);
 
     Route::post('change-password', [UserController::class, 'changePassword']);
+
+    Route::get('favourites', [FavouriteController::class, 'getFavourites']);
 
     Route::prefix('comments')->middleware(['auth:sanctum'])->group(function () {
 
@@ -54,6 +57,10 @@ Route::prefix('kebabs')->group(function () {
     Route::get('{kebab}/comments', [CommentController::class, 'getCommentsByKebabId']);
 
     Route::middleware(['auth:sanctum'])->post('{kebab}/comments', [CommentController::class, 'addComment']);
+
+    Route::middleware(['auth:sanctum'])->post('{kebab}/favourite', [FavouriteController::class, 'addToFavourites']);
+
+    Route::middleware(['auth:sanctum'])->delete('{kebab}/favourite', [FavouriteController::class, 'removeFromFavourites']);
 
     Route::middleware(['auth:sanctum', 'admin'])->post('/', [KebabController::class, 'store']);
 
