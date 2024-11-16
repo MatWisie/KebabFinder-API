f53f434cdbb45d4ea676b9d4cd84d68b1197cc54
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-16 21:15:09 +0100

    Service for adding kebab to favourites

--- KebabBackend/app/Services/FavouriteService.php
+++ KebabBackend/app/Services/FavouriteService.php
@@ -7,4 +7,17 @@ use App\Models\User;
 
 class FavouriteService
 {
+    /**
+     * Add a kebab to the user's favourites.
+     */
+    public function addFavourite(User $user, Kebab $kebab): bool
+    {
+        if ($this->isFavourite($user, $kebab)) {
+            return false;
+        }
+
+        $user->favouriteKebabs()->attach($kebab->id);
+        return true;
+    }
+
 }
