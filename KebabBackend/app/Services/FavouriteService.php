9b7644a3c95c9b052d5f71663b91624dee9082bb
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-30 12:43:38 +0100

    Revert "Service for getting users kebabs from favourites"
    This reverts commit c936a5e0015ba4ee7f49a3992524672eb1fb4b42.

--- KebabBackend/app/Services/FavouriteService.php
+++ KebabBackend/app/Services/FavouriteService.php
@@ -33,12 +33,4 @@ class FavouriteService
         return true;
     }
 
-    /**
-     * Get the user's favourite kebabs.
-     */
-    public function getFavourites(User $user, int $perPage = 10)
-    {
-        return $user->favouriteKebabs()->paginate($perPage);
-    }
-
 }
