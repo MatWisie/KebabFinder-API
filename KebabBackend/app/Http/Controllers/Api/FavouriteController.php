aebdd1994cd3dbd4e55b78fc7019f98a0c431598
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-30 12:43:33 +0100

    Revert "imports for favourite controller"
    This reverts commit f526540dba3055fdb3cdd81b685e7bb4f5387d0e.

--- KebabBackend/app/Http/Controllers/Api/FavouriteController.php
+++ KebabBackend/app/Http/Controllers/Api/FavouriteController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Kebab;
-use Illuminate\Http\JsonResponse;
-use App\Services\FavouriteService;
 
 class FavouriteController extends Controller
 {
