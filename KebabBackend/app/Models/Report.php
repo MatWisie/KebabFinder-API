312e44b1ff1ec065aff83528c2b112d5f8d6ee8c
W-Worach <115943399+W-Worach@users.noreply.github.com>
2024-11-30 14:51:01 +0100

    added fillables to report model

--- KebabBackend/app/Models/Report.php
+++ KebabBackend/app/Models/Report.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
+    protected $fillable = [
+        'kebab_id',
+        'message',
+        'status',
+    ];
+
     public function kebab()
     {
         return $this->belongsTo(Kebab::class);
