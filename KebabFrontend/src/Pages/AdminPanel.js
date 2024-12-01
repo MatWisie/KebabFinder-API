e807254a90400511c84754cecd7a2613fc9a7b90
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-12-01 17:45:13 +0100

    changed comment layout, added deleting comments

--- KebabFrontend/src/Pages/AdminPanel.js
+++ KebabFrontend/src/Pages/AdminPanel.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import L from 'leaflet'
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import { UserContext } from '../Contexts/AuthContext';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -21,6 +22,7 @@ export default function AdminPanel(){
   const [loadingKebabs, setLoadingKebabs] = useState(false)
   const [loadingComments, setLoadingComments] = useState(false)
   const [comments, setComments] = useState([])
+  const {token, isLoading} = useContext(UserContext)
     
     useEffect(() => {
       if (!mapRef.current) return;
@@ -87,8 +89,7 @@ export default function AdminPanel(){
               const button = document.getElementById(`button-${kebab.id}`);
               if (button) {
                 button.addEventListener('click', () => {
-                  clickedKebabID = kebab.id
-                  showPanel();
+                  showPanel(kebab.id);
                 });
               }
             });
@@ -127,7 +128,6 @@ export default function AdminPanel(){
 
     async function getKebabComments(kebabID){
       setLoadingComments(true)
-      setComments([])
       try {
         const response = await fetch(apiUrl + `kebabs/${kebabID}/comments`, {
           method: 'GET',
@@ -139,7 +139,7 @@ export default function AdminPanel(){
 
         if (!response.ok) {
           setLoadingComments(false)
-          throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not ok ' + response.statusText)
         }
         const data = await response.json()
         setComments(data)
@@ -156,28 +156,68 @@ export default function AdminPanel(){
 
     const [isPanelVisible, setIsPaneVisible] = useState(false)
 
-    function showPanel() {
+    function showPanel(kebab_id) {
+
+      setClickedKebabID(kebab_id)
+
       setIsPaneVisible(true)
       
-      setChosenKebab(kebabs.find(kebab => kebab.id === clickedKebabID))
-      getKebabComments(clickedKebabID)
+      setChosenKebab(kebabs.find(kebab => kebab.id === kebab_id))
+      getKebabComments(kebab_id)
     }
     function hidePanel() {
       setIsPaneVisible(false)
     }
 
-    var clickedKebabID = 0
+    const [clickedKebabID, setClickedKebabID] = useState(0)
+
     const [chosenKebab, setChosenKebab] = useState(null)
 
     const [isSidePanelOpen, setIsSidePanelOpen] = useState(false)
     const togglePanel = () => setIsSidePanelOpen(!isSidePanelOpen)
+
+    const [isBeingDeleted, setIsBeingDeleted] = useState({})
+    async function handleDeleteComment(comment_id) {
+
+      setIsBeingDeleted((prev) => ({ ...prev, [comment_id]: true }))
+
+      try {
+        const response = await fetch(apiUrl + `admin/delete-comment/${comment_id}`, {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
+        });
+    
+        if (!response.ok) {
+          throw new Error('Network response was not ok: ' + response.statusText);
+        }
+    
+        if (response.status !== 204) {
+          var data = await response.json()
+          console.log('Response Data:', data)
+        } else {
+          console.log('Comment Deleted')
+          await getKebabComments(clickedKebabID);
+        }
+    
+      } catch (error) {
+        console.log('Error deleting comment: ', error)
+
+      } finally {
+        setIsBeingDeleted((prev) => ({ ...prev, [comment_id]: false }))
+
+      }
+    }
 
     return (
         <div className="flex h-screen overflow-hidden">
           {isSidePanelOpen &&
           <div id="sidePanel" className={`h-full z-30 bg-white overflow-y-auto shadow-lg ${isSidePanelOpen ? "w-full sm:w-2/5 md:w-1/5" : "hidden"}`}>
             <div id="searchBar" className="w-full">
-              <input type="text" placeholder="Search" className=" my-2 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-3/4"></input>
+              <input type="text" placeholder="Search" className="my-2 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-3/4"></input>
             </div>
             <div className="kebabList">
               {loadingKebabs && <div>Loading kebabs...</div>}
@@ -189,9 +229,7 @@ export default function AdminPanel(){
                   <address>{kebab.address}</address>
                   <div>{kebab.status}</div>
                   <button onClick={()=>{
-                    clickedKebabID = kebab.id
-                    showPanel()
-                    
+                    showPanel(kebab.id)
                     }}>Show More</button>
                 </div>
               )) : <></>}
@@ -208,13 +246,20 @@ export default function AdminPanel(){
             <div id="map" className="w-full h-full" ref={mapRef} ></div>
           </div>
           {isPanelVisible && 
-            <KebabPanel onAction={hidePanel} kebab={chosenKebab} comments={comments} loadingComments={loadingComments}></KebabPanel>
+            <KebabPanel 
+              onAction={hidePanel} 
+              kebab={chosenKebab} 
+              comments={comments} 
+              loadingComments={loadingComments}
+              onDelete={handleDeleteComment}
+              isBeingDeleted={isBeingDeleted}
+              />
           }
         </div>
     )
 }
 
-function  KebabPanel({ onAction, kebab, comments, loadingComments }) {
+function  KebabPanel({ onAction, kebab, comments, loadingComments, onDelete, isBeingDeleted }) {
 
   return (
     <div className="right-0 fixed flex justify-center items-center bg-gray-500 z-50 w-full h-full bg-opacity-70">
@@ -313,15 +358,29 @@ function  KebabPanel({ onAction, kebab, comments, loadingComments }) {
                     comments.length > 0 ?
                       <ul>
                         {comments.map((comment, index) => (
-                          <li key={index}>
-                            <div>{comment.user.name}</div>
-                            <p>{comment.content}</p>
+                          <li key={index} className="mb-4 flex flex-col sm:flex-row items-start sm:justify-between">
+                            <div className="flex w-full sm:w-auto mb-2 sm:mb-0">
+                              <div className="flex-shrink-0 w-12 h-12 bg-gray-300 text-gray-800 rounded-full flex items-center justify-center font-bold mr-4">
+                                {comment.user.name.charAt(0).toUpperCase()}
+                              </div>
+                              <div className="flex-1">
+                                <div className="font-bold text-gray-800 text-left text-wrap">{comment.user.name}</div>
+                                <p className="text-gray-600 mt-1 text-wrap break-all">{comment.content}</p>
+                              </div>
+                            </div>
+                            <button
+                              id={`delete-comment-${comment.id}`}
+                              className="text-red-600 hover:bg-red-500 hover:text-white text-sm font-medium p-2 border-2 border-red-500 self-end sm:self-auto"
+                              onClick={() => onDelete(comment.id)}
+                              disabled={isBeingDeleted[comment.id]}
+                            >
+                              {isBeingDeleted[comment.id] ? 'Deleting...' : 'Delete Comment'}
+                            </button>
                           </li>
                         ))}
                       </ul>
                       :
                       <div>No comments</div>
-                    
                   }
                 </details>
                 </div>
