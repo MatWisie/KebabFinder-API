0f56ab4775570519c33d53aa97b8a1e49839fbf9
W-Worach <115943399+W-Worach@users.noreply.github.com>
2025-01-14 19:18:40 +0100

    controller for getting kebab rating from google

--- KebabBackend/app/Http/Controllers/Api/GooglePlacesController.php
+++ KebabBackend/app/Http/Controllers/Api/GooglePlacesController.php
@@ -9,4 +9,34 @@ use Illuminate\Http\JsonResponse;
 
 class GooglePlacesController extends Controller
 {
+    protected GoogleApiService $googleApiService;
+
+    public function __construct(GoogleApiService $googleApiService)
+    {
+        $this->googleApiService = $googleApiService;
+    }
+
+    public function getKebabDetails(Kebab $kebab): JsonResponse
+    {
+        $name = $kebab->name;
+        $address = $kebab->address;
+
+        if (empty($name) || empty($address)) {
+            return response()->json(['message' => 'Invalid kebab data'], 400);
+        }
+
+        $rating = $this->googleApiService->fetchRatingByNameAndAddress($name, $address);
+
+        if ($rating === 0) {
+            return response()->json(['message' => 'Restaurant not found'], 404);
+        }
+
+        $kebab->google_review = $rating;
+        $kebab->save();
+
+        return response()->json([
+            'name' => $name,
+            'rating' => $rating,
+        ], 200);
+    }
 }
