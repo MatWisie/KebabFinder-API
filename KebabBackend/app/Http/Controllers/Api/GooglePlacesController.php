d5038cc84c6465e8d82030526a5a56fef688bfb3
W-Worach <115943399+W-Worach@users.noreply.github.com>
2025-01-14 19:18:17 +0100

    Create GooglePlacesController.php

---
+++ KebabBackend/app/Http/Controllers/Api/GooglePlacesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Kebab;
+use App\Services\GoogleApiService;
+use Illuminate\Http\JsonResponse;
+
+class GooglePlacesController extends Controller
+{
+}
